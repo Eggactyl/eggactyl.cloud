e00031f0278c709e8c5201727724f37e7ef4ab82
Shane Caffoe <shane@scaffoe.com>
2023-09-20 00:33:08 +0000

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,55 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog';
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://eggactyl.cloud",
+	integrations: [
+		starlightBlog({
+			title: "Release Notes",
+			authors: {
+				shane: {
+					name: "Shane C.",
+					title: "Developer",
+					picture: "/egg-icon.webp",
+					url: "https://eggactyl.cloud"
+				}
+			}
+		}),
+		starlight({
+			title: 'Eggactyl',
+			social: {
+				github: 'https://github.com/eggactyl',
+				discord: 'https://discord.eggactyl.cloud'
+			},
+			logo: {
+				src: "./src/assets/egg-icon.webp"
+			},
+			sidebar: [
+				{ 
+					label: "Welcome", 
+					items: [
+						{ label: "Introduction", link: "/introduction" },
+						{ label: "Configuration", link: "/configuration" },
+						{ label: "Environment Variables", link: "/environment" },
+						{ label: "Flags", link: "/flags" },
+						{ label: "FAQ", link: "/faq" },
+					]
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Installation', link: '/guides/installation' },
+					],
+				},
+				{
+					label: "Legal",
+					autogenerate: { directory: "legal" }
+				}
+			],
+			customCss: ['./src/assets/landing.css']
+		}),
+	],
+});
---
+++ packages/starlight-blog/libs/page.ts
@@ -0,0 +1,40 @@
+import type { AstroGlobal } from 'astro'
+import config from 'virtual:starlight-blog-config'
+
+export function isAnyBlogPage({ url }: AstroGlobal) {
+  return url.pathname.match(/^\/releases(\/?$|\/.+\/?$)/)
+}
+
+export function isBlogRoot({ url }: AstroGlobal) {
+  return url.pathname.match(/^\/releases\/?$/)
+}
+
+export function isAnyBlogPostPage({ url }: AstroGlobal) {
+  return url.pathname.match(/^\/releases\/(?!tags\/.+).+$/) !== null;
+}
+
+export function isBlogPostPage({ url }: AstroGlobal, slug: string) {
+  return url.pathname.match(new RegExp(`^/${slug}/?$`)) !== null
+}
+
+export function isBlogTagsPage({ url }: AstroGlobal, tag: string) {
+  return url.pathname.match(new RegExp(`^/releases/tags/${tag}/?$`)) !== null
+}
+
+export function getPageProps(title: string, slug: string) {
+  return {
+    entry: {
+      data: {
+        editUrl: false,
+        head: [],
+        tableOfContents: false,
+        title,
+      },
+      slug,
+    },
+    entryMeta: {
+      lang: config.locale,
+    },
+    lang: config.locale,
+  }
+}
