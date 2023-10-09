0f4f4f4e22c1a4dc3ce92d7cb97c14399742d192
Shane Caffoe <shane@scaffoe.com>
2023-10-09 13:55:22 +0000

    Added Indonesian

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,34 +24,68 @@ export default defineConfig({
     logo: {
       src: "./src/assets/egg-icon.svg"
     },
+    defaultLocale: "en",
+    locales: {
+      en: {
+        label: "English"
+      },
+      in: {
+        label: "Bahasa Indonesia",
+        lang: "in"
+      }
+    },
     sidebar: [{
       label: "Welcome",
+      translations: {
+        in: "Selamat Datang"
+      },
       items: [{
         label: "Introduction",
+        translations: {
+          "in": "Perkenalan"
+        },
         link: "/introduction"
       }, {
         label: "Configuration",
+        translations: {
+          in: "Konfigurasi"
+        },
         link: "/configuration"
       }, {
         label: "Environment Variables",
+        translations: {
+          in: "Variabel Lingkungan",
+        },
         link: "/environment"
       }, {
-        label: "Flags",
+        label: "Flag",
         link: "/flags"
       }, {
         label: "FAQ",
+        translations: {
+          in: "Pertanyaan Umum"
+        },
         link: "/faq"
       }]
     }, {
       label: 'Guides',
+      translations: {
+        in: "Panduan"
+      },
       items: [
       // Each item here is one entry in the navigation menu.
       {
         label: 'Installation',
+        translations: {
+          in: "Instalasi"
+        },
         link: '/guides/installation'
       }]
     }, {
       label: "Legal",
+      translations: {
+        in: "Hukum"
+      },
       autogenerate: {
         directory: "legal"
       }
