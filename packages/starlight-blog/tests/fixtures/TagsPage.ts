18eef20d7e8f54f5437529de286551b3571d0adf
Shane C. <shane@scaffoe.com>
2024-06-08 20:35:22 -0400

    remove unneeded package

--- packages/starlight-blog/tests/fixtures/TagsPage.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from "@playwright/test";
-
-export class TagsPage {
-  constructor(public readonly page: Page) {}
-
-  goto(tag: string) {
-    return this.page.goto(`/blog/tags/${tag}`);
-  }
-}
--- packages/starlight-blog/virtual.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "virtual:starlight-blog-config" {
-  const StarlightBlogConfig: import("./libs/config").StarlightBlogConfig;
-
-  export default StarlightBlogConfig;
-}
